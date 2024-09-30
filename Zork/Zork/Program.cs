bf592cd9728f33dddffda04321029c40fd2523be
Smug <zikadika2@gmail.com>
2024-09-30 13:52:46 -0400

    Step 4

--- Zork/Zork/Program.cs
+++ Zork/Zork/Program.cs
@@ -6,26 +6,22 @@ using System.Threading.Tasks;
 
 namespace Zork
 {
-    internal class Program
+
+    class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Zork!");
 
             string inputString = Console.ReadLine();
-            inputString = inputString.ToUpper();
-            if(inputString == "Quit")
-            {
-                Console.WriteLine("Thank you for playing.");
-            }
-            else if (inputString == "LOOK")
-            {
-                Console.WriteLine("This is an open field of a white house, with a boarded door. \nA rubber mat saying 'Welcoe to Zork!' lies by the door.");
-            }
-            else
-            {
-                Console.WriteLine("Unrecognized command");
-            }
+            Commands command = ToCommand(inputString.Trim().ToUpper());
+            Console.WriteLine(command);
+        }
+
+        private static Commands ToCommand(string commandString)
+        {
+            return Enum.TryParse<Commands>(commandString, true, out Commands result) ? result : Commands.UNKOWN;
         }
     }
 }
+
