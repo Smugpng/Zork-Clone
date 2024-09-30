b3537dea3c14173a871636c3ffd8bc2bbcea2d36
Smug <zikadika2@gmail.com>
2024-09-30 14:25:05 -0400

    Step 5

--- Zork/Zork/Program.cs
+++ Zork/Zork/Program.cs
@@ -13,15 +13,50 @@ namespace Zork
         {
             Console.WriteLine("Welcome to Zork!");
 
-            string inputString = Console.ReadLine();
-            Commands command = ToCommand(inputString.Trim().ToUpper());
-            Console.WriteLine(command);
+            Commands command = Commands.UNKOWN;
+            while (command != Commands.QUIT)
+            {
+                Console.Write("> ");
+                command = ToCommand(Console.ReadLine().Trim());
+
+                string outputString;
+                switch (command)
+                {
+                    case Commands.NORTH:
+                        outputString = "You Moved NORTH";
+                        break;
+
+                    case Commands.SOUTH:
+                        outputString = "You moved SOUTH";
+                        break;
+
+                    case Commands.EAST:
+                        outputString = "You moved EAST";
+                        break;
+
+                    case Commands.WEST:
+                        outputString = "You moved WEST";
+                        break;
+
+                    case Commands.UNKOWN:
+                        outputString = "HELLO?";
+                        break;
+                    case Commands.LOOK:
+                        outputString = "A Rubber mat saying 'Welcome To Zork!' lies by the door.";
+                        break;
+
+                    default:
+                        outputString = "Thanks For Playing";
+                        break ;
+                     
+                }
+
+                Console.WriteLine(outputString);
+
+            }   
         }
 
-        private static Commands ToCommand(string commandString)
-        {
-            return Enum.TryParse<Commands>(commandString, true, out Commands result) ? result : Commands.UNKOWN;
-        }
+        private static Commands ToCommand(string commandString) => (Enum.TryParse<Commands>(commandString, true, out Commands result) ? result : Commands.UNKOWN);
     }
 }
 
