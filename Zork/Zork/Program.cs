31b1ff33baf4ce2a1023a0ba7cfa26ad1f4d98e4
Smug <zikadika2@gmail.com>
2024-09-30 13:35:42 -0400

    Step 3

--- Zork/Zork/Program.cs
+++ Zork/Zork/Program.cs
@@ -10,7 +10,22 @@ namespace Zork
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to Zork!");
+
+            string inputString = Console.ReadLine();
+            inputString = inputString.ToUpper();
+            if(inputString == "Quit")
+            {
+                Console.WriteLine("Thank you for playing.");
+            }
+            else if (inputString == "LOOK")
+            {
+                Console.WriteLine("This is an open field of a white house, with a boarded door. \nA rubber mat saying 'Welcoe to Zork!' lies by the door.");
+            }
+            else
+            {
+                Console.WriteLine("Unrecognized command");
+            }
         }
     }
 }
